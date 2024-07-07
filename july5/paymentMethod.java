5b4f7331d42058e4c1540eef379fa2ca941c7fbf
mercinati <119480406+mercinati@users.noreply.github.com>
2024-07-08 01:35:49 +0530

    Add files via upload

---
+++ july5/paymentMethod.java
@@ -0,0 +1,36 @@
+package july5;
+
+public interface paymentMethod {
+    public void pay();
+    public void getPaymentDetails();
+}
+
+class creditCard implements paymentMethod {
+    long CN;String date;
+    public creditCard(long CN,String date) {
+        this.CN = CN;
+        this.date = date;
+    }
+    public void pay() {
+        System.out.println("you are payying to Karan");
+    }
+    public void getPaymentDetails() {
+        System.out.println("your payment details are: card no. :"+CN+", Expiring on:"+date);
+    }
+}
+
+class paypal implements paymentMethod {
+    String e_mail;
+    long CN;String date;
+    public paypal(String e_mail,long CN,String date) {
+        this.e_mail= e_mail;
+        this.CN = CN;
+        this.date = date;
+    }
+    public void pay() {
+        System.out.println("you are payying to Karan");
+    }
+    public void getPaymentDetails() {
+        System.out.println("your payment details are: card no. :"+CN+", Expiring on:"+date+", your E-mail is: "+e_mail);
+    }
+}
---
+++ july5/printable.java
@@ -0,0 +1,27 @@
+package july5;
+
+public interface printable {
+    public void print();
+    
+}
+
+class document implements printable {
+    String txt;
+    public document(String txt) {
+        this.txt = txt;
+    }
+    public void print() {
+        System.out.println("text field is: "+txt);
+    }
+}
+
+class image implements printable {
+    int w, h;
+    public image(int h, int w) {
+        this.w = w;
+        this.h = h;
+    }
+    public void print() {
+        System.out.println("height is: "+h+", width is: "+w);
+    }
+}
